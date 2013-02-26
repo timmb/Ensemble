638bf9954f47c108520f3ff7c51b72c542142d1d
Tim <tim@timmb.com>
2013-02-26 15:33:32 +0000

    Removed xn dependency from user.cpp

--- KinectStreamer/src/User.cpp
+++ KinectStreamer/src/User.cpp
@@ -12,7 +12,7 @@
 #include <boost/assign.hpp>
 //#include "tmb/Utilities.h"
 #include "cinder/Utilities.h"
-#include <XnTypes.h>
+//#include <XnTypes.h>
 
 using namespace ci;
 using namespace std;
@@ -23,6 +23,32 @@ using namespace std;
 const int NUM_JOINTS = 15;
 
 namespace {
+	// Redefine OpenNI joint enumeration as integers
+	const int XN_SKEL_HEAD			= 1;
+	const int XN_SKEL_NECK			= 2;
+	const int XN_SKEL_TORSO			= 3;
+	const int XN_SKEL_WAIST			= 4;
+	const int XN_SKEL_LEFT_COLLAR		= 5;
+	const int XN_SKEL_LEFT_SHOULDER	= 6;
+	const int XN_SKEL_LEFT_ELBOW		= 7;
+	const int XN_SKEL_LEFT_WRIST		= 8;
+	const int XN_SKEL_LEFT_HAND		= 9;
+	const int XN_SKEL_LEFT_FINGERTIP	=10;
+	const int XN_SKEL_RIGHT_COLLAR	=11;
+	const int XN_SKEL_RIGHT_SHOULDER	=12;
+	const int XN_SKEL_RIGHT_ELBOW		=13;
+	const int XN_SKEL_RIGHT_WRIST		=14;
+	const int XN_SKEL_RIGHT_HAND		=15;
+	const int XN_SKEL_RIGHT_FINGERTIP	=16;
+	const int XN_SKEL_LEFT_HIP		=17;
+	const int XN_SKEL_LEFT_KNEE		=18;
+	const int XN_SKEL_LEFT_ANKLE		=19;
+	const int XN_SKEL_LEFT_FOOT		=20;
+	const int XN_SKEL_RIGHT_HIP		=21;
+	const int XN_SKEL_RIGHT_KNEE		=22;
+	const int XN_SKEL_RIGHT_ANKLE		=23;
+	const int XN_SKEL_RIGHT_FOOT		=24;
+	
 	XnSkeletonJointId ____activeJoints[NUM_JOINTS] = { XN_SKEL_HEAD, XN_SKEL_NECK, XN_SKEL_TORSO, XN_SKEL_LEFT_SHOULDER, XN_SKEL_LEFT_ELBOW, XN_SKEL_LEFT_HAND, XN_SKEL_RIGHT_SHOULDER, XN_SKEL_RIGHT_ELBOW, XN_SKEL_RIGHT_HAND, XN_SKEL_LEFT_HIP, XN_SKEL_LEFT_KNEE, XN_SKEL_LEFT_FOOT, XN_SKEL_RIGHT_HIP, XN_SKEL_RIGHT_KNEE, XN_SKEL_RIGHT_FOOT };
 	std::string ____activeJointNames[NUM_JOINTS] = { "head", "neck", "torso", "left_shoulder", "left_elbow", "left_hand", "right_shoulder", "right_elbow", "right_hand", "left_hip", "left_knee", "left_foot", "right_hip", "right_knee", "right_foot" };
 	bool ____isRight[NUM_JOINTS] = { 0,0,0,0,0,0,1,1,1,0,0,0,1,1,1 };
