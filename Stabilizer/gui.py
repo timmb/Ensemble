e66d1cb79b71c7f3c57917dce27ff75302cec139
Tim <tim@timmb.com>
2013-03-21 18:14:10 +0000

    More work on gui

--- Stabilizer/gui.py
+++ Stabilizer/gui.py
@@ -3,19 +3,22 @@ This module contains code to hook up the GUI (generated using the Qt UI
 designer) to the rest of the code and any hand-written GUI code.
 '''
 
-from UX import mainWindows
+
+import UX.MainWindow
 from PySide import QtCore, QtGui
 from PySide.QtGui import *
+
+
 
 class MainWindow(QtGui.QMainWindow):
     def __init__(self, stabilizer, parent = None):
         QtGui.QMainWindow.__init__(self, parent)
-        self.ui = mainWindows.Ui_MainWindow()
+        self.ui = UX.MainWindow.Ui_MainWindow()
         self.ui.setupUi(self)
         self.stabilizer = stabilizer
         
-        self.ui.listView.setModel(stabilizer.event_log)
-        self.ui.pushButton.clicked.connect(self.start_or_stop_listening)
+        self.ui.logView.setModel(stabilizer.event_log)
+        self.ui.startOrStopListeningButton.clicked.connect(self.start_or_stop_listening)
         self.ui.actionQuit.triggered.connect(stabilizer.quit)
 
         self.show()
@@ -23,7 +26,7 @@ class MainWindow(QtGui.QMainWindow):
     def start_or_stop_listening(self):
         if self.stabilizer.is_listening:
             self.stabilizer.stop_listening()
-            self.ui.pushButton.setText("Start Listening")
+            self.ui.startOrStopListeningButton.setText("Start Listening")
         else:
             self.stabilizer.start_listening()
-            self.ui.pushButton.setText("Stop Listening")
+            self.ui.startOrStopListeningButton.setText("Stop Listening")
