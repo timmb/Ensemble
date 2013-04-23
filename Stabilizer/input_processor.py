dbfcfffe227a6163b28d36e1030fc600cc0bfb60
Tim <tim@timmb.com>
2013-04-23 20:37:07 +0100

    Fixed some bugs in input processor. Added validity check to input arguments. Added per-instrument state storage for gui.

--- Stabilizer/input_processor.py
+++ Stabilizer/input_processor.py
@@ -1,4 +1,5 @@
 import re
+import time
 
 class InputProcessor(object):
 	'''The Input Processor receives OSC messages and updates the world state accordingly.
@@ -12,6 +13,7 @@ class InputProcessor(object):
 		self.world_state = world_state
 		self.instruments = instruments
 		self.log = log_function
+		
 		self.state_pattern = re.compile(
 			r'''
 			 / state / (?P<sender> [^/]+) / (?P<parameter> [^/]+)
@@ -20,38 +22,69 @@ class InputProcessor(object):
 			r'''
 			/ (?P<type> (log|warning|error)) / (?P<sender> [^/]+)
 		''', re.VERBOSE)
-		self.ping_pattern = re.compile(r'/ ping / (<?P<sender> [^/]+)', re.VERBOSE)
-
-	def we_have_heard_from(self, instrument_name):
-		if instrument_name not in self.instruments:
-			self.log('First message received from '+instrument_name, 'InputProcessor')
-		self.instruments[instrument_name] = time.strftime("%H:%M:%S.%f")
+		self.ping_pattern = re.compile(r'/ ping / (?P<sender> [^/]+)', re.VERBOSE)
+		
+		type_tags_regex = {
+			'activity' : r'f',
+			'tempo' : r'f',
+			'loudness' : r'f',
+			'immediate_pitch' : r'i+',
+			'root' : r'i',
+			'harmony' : r'i+',
+			'detune' : r'f',
+			'note_on' : r'fi?',
+			'note_frequency' : r'f',
+			'note_density' : r'f',
+			'attack' : r'f',
+			'brightness' : r'f',
+			'roughness' : r'f',
+		}
+		self.valid_message_type_tags = {
+			param : re.compile(type_tags_regex[param]) for param in type_tags_regex
+		}
 
 	def osc_message_callback(self, message, origin):
 		'''message is of type: txosc.osc.Message.
 		origin should be in the form (host, port)
 		'''
 		# to do
-		log_match = self.log_pattern(message.address)
-		state_match = self.state_pattern(message.address)
-		ping_match = self.ping_pattern(message.address)
+		log_match = self.log_pattern.match(message.address)
+		state_match = self.state_pattern.match(message.address)
+		ping_match = self.ping_pattern.match(message.address)
 		match = log_match or state_match or ping_match
 		
 		if not match:
-			self.log("Invalid OSC message received from {}: {}", "InputProcessor")
+			self.log("Invalid OSC message received from {}: {}".format(origin, message), "InputProcessor")
 			return
 		sender = match.group('sender')
-		we_have_heard_from(sender)
+		self.we_have_heard_from(sender)
 
 		if log_match:
 			log_message = ', '.join(map(str, message.getValues()))
 			self.log(log_match.group('type')+': '+log_message, sender)
-		else:
+		elif state_match:
 			param = state_match.group('parameter')
 			value = message.getValues()
-			check_valid(param, value)
-			if param not in self.world_state:
-				self.log('First value received for '+param, 'InputProcessor')
-				self.world_state[param] = {}
-			self.world_state[param][sender] = values
+			if self.check_valid(param, message.getTypeTags(), value):
+				if param not in self.world_state:
+					self.log('First value received for '+param, 'InputProcessor')
+					self.world_state[param] = {}
+				self.world_state[param][sender] = value
+				self.instruments[sender]['state'][param] = value
+			else:
+				self.log('Invalid parameter/value combination from '
+					+'{} for parameter {}: {}'.format(sender, param, value))
+
+	def we_have_heard_from(self, instrument_name):
+		if instrument_name not in self.instruments:
+			self.log('First message received from '+instrument_name, 'InputProcessor')
+			self.instruments[instrument_name] = {}
+			self.instruments[instrument_name]['state'] = {}
+		self.instruments[instrument_name]['last heard at'] = time.strftime("%H:%M:%S")
+
+	def check_valid(self, param, type_tags, arguments):
+		if param in self.valid_message_type_tags:
+			return self.valid_message_type_tags[param].match(type_tags)
+		else:
+			return True
 
